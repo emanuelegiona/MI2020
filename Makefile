6a81293a3dd49a242cfa02b46390567d8567e14f
Emanuele Giona <giona.emanuele@gmail.com>
2020-06-12 18:43:17 +0200

    Google MediaPipe interaction implemented

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-all: libs pip
+all: libs pip mediapipe
 
 libs:
 	@echo "\nInstalling required packets using APT\n"
@@ -7,8 +7,34 @@ libs:
 pip:
 	@echo "\nInstalling Python packages using PIP\n"
 	pip install numpy
+	pip install opencv-python
+	pip install tensorflow
+	pip install shapely
+	pip install scipy
+	pip install pillow
+	pip install imageio
+	pip install imageio-ffmpeg
+	pip install matplotlib
 	pip install soundfile
 	pip install sounddevice
 	pip install pydub
 	pip install requests
 	pip install appjar
+
+mediapipe:
+    @echo "\nInstalling dependencies for Google MediaPipe\n"
+    curl https://bazel.build/bazel-release.pub.gpg | sudo apt-key add -
+    echo "deb [arch=amd64] https://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
+    sudo apt update && sudo apt install bazel
+    sudo apt install libopencv-core-dev libopencv-highgui-dev libopencv-calib3d-dev libopencv-features2d-dev libopencv-imgproc-dev libopencv-video-dev
+    @echo "\nCloning Google MediaPipe from its GitHub repository\n"
+    cd ..
+    git clone https://github.com/google/mediapipe.git
+    @echo "\nReplacing original Google MediaPipe files with custom ones (credits: github.com/rabBit64)\n"
+    rm mediapipe/mediapipe/calculators/core/end_loop_calculator.h
+    cp MI2020/data/mediapipe_custom/end_loop_calculator.h mediapipe/mediapipe/calculators/core/
+    rm mediapipe/mediapipe/calculators/util/landmarks_to_render_data_calculator.cc
+    cp MI2020/data/mediapipe_custom/landmarks_to_render_data_calculator.cc mediapipe/mediapipe/calculators/util/
+    rm mediapipe/mediapipe/examples/desktop/demo_run_graph_main.cc
+    cp MI2020/data/mediapipe_custom/demo_run_graph_main.cc mediapipe/mediapipe/examples/desktop/
+    cd MI2020
---
+++ backend/mediapipe/mediapipe.py
@@ -0,0 +1,88 @@
+"""
+This file contains a Python wrapper to interact with a Google MediaPipe previously installed (see config.json file).
+"""
+
+import os
+import json
+
+# Global variables targeting the multi-hand tracking task in MediaPipe
+MEDIAPIPE_SUBPATH = "mediapipe/examples/desktop/multi_hand_tracking"
+TARGET = "multi_hand_tracking_cpu"
+COMPILE_STR = "bazel build -c opt --define MEDIAPIPE_DISABLE_GPU=1 {path}:{target}"
+EXEC_STR = "GLOG_logtostderr=1 {runnable} --calculator_graph_config_file={calculator}"
+
+
+class MediaPipeHelper:
+
+    def __init__(self, mediapipe_dir: str):
+        """
+        Helper class to run Google MediaPipe from Python.
+        :param mediapipe_dir: Absolute directory pointing to the installation directory of MediaPipe
+        """
+
+        if not os.path.exists(mediapipe_dir):
+            raise FileNotFoundError("Invalid MediaPipe installation directory.")
+        elif not os.path.isabs(mediapipe_dir):
+            raise ValueError("An absolute path must be provided.")
+        elif not os.path.isdir(mediapipe_dir):
+            raise NotADirectoryError("The provided path is not a directory.")
+
+        contents = os.listdir(mediapipe_dir)
+        if "WORKSPACE" not in contents:
+            raise ValueError("MediaPipe installation directory must contain a WORKSPACE file.")
+
+        self.__mediapipe_dir = mediapipe_dir
+        self.__compile_str = COMPILE_STR.format(path=MEDIAPIPE_SUBPATH,
+                                                target=TARGET)
+        self.__exec_str = EXEC_STR.format(runnable=os.path.join(self.__mediapipe_dir,
+                                                                "bazel-bin",
+                                                                MEDIAPIPE_SUBPATH,
+                                                                TARGET),
+                                          calculator=os.path.join(self.__mediapipe_dir,
+                                                                  "mediapipe",
+                                                                  "graphs",
+                                                                  "hand_tracking",
+                                                                  "multi_hand_tracking_desktop_live.pbtxt"))
+
+        # Moving to the MediaPipe's working dir
+        curr_wd = os.getcwd()
+        os.chdir(self.__mediapipe_dir)
+        # Compiling MediaPipe's graph for multi-hand tracking
+        os.system(self.__compile_str)
+        # Restoring GesturePad's working dir
+        os.chdir(curr_wd)
+
+    def run(self, input_dir: str, output_dir: str) -> None:
+        """
+        Executes MediaPipe on the given input video exporting its results to the chosen path.
+        :param input_dir: Path to the input video
+        :param output_dir: Path to the output video to produce
+        :return: None
+        """
+
+        command = "{exec} --input_video_path={input_dir} --output_video_path={output_dir}"
+        command = command.format(exec=self.__exec_str,
+                                 input_dir=input_dir,
+                                 output_dir=output_dir)
+
+        # Moving to the MediaPipe's working dir
+        curr_wd = os.getcwd()
+        os.chdir(self.__mediapipe_dir)
+        # Compiling MediaPipe's graph for multi-hand tracking
+        os.system(command)
+        # Restoring GesturePad's working dir
+        os.chdir(curr_wd)
+
+
+if __name__ == '__main__':
+    with open("../../data/config.json") as config_f:
+        config = json.load(config_f)
+    mediapipe_inst = config["mediapipe_dir"]
+    print(mediapipe_inst)
+
+    print("helper creation")
+    helper = MediaPipeHelper(mediapipe_dir=mediapipe_inst)
+
+    print("running on test.mp4")
+    helper.run(input_dir="/home/emanuele/source/mi/MI2020/tmp/test.mp4",
+               output_dir="/home/emanuele/source/mi/MI2020/tmp/test_out.mp4")
