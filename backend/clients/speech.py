fd573eba360b971e67fa94d16cb5e976aa3449ea
Emanuele Giona <giona.emanuele@gmail.com>
2020-06-15 11:10:24 +0200

    setting auth environment variables for gcloud wrapper classes

--- backend/clients/speech.py
+++ backend/clients/speech.py
@@ -2,12 +2,21 @@
 This file contains the SpeechClient definition.
 """
 
+import os
+from utils.config_helper import read_config
 from rest_client import RESTClient, SimpleResponse
 
 
 class SpeechClient:
-    pass
+
+    def __init__(self):
+        # Let Google Cloud libraries pick up credentials from environment variables
+        config, _ = read_config()
+        os.environ["PROJECT_ID"] = config["project_id"]
+        os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = config["credentials"]
+        pass
 
 
 if __name__ == '__main__':
-    pass
+    c = SpeechClient()
+    print(os.environ["PROJECT_ID"])
